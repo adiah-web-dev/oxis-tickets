8dd3359bec793cd0f1abd7d18667cf076ed63d78
Nathaniel Adiah <nathanieladiah@gmail.com>
2025-05-09 04:07:30 -0400

    Add the theming files

--- ticket_generator/settings.py
+++ ticket_generator/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ticket_generator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
