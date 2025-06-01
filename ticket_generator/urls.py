6238004e0429924840fb6b7b0165dde1eaca53db
Nathaniel Adiah <nathanieladiah@gmail.com>
2025-06-01 14:24:02 -0400

    Add a scanner app
    Create a ssl version of the website so it can be accessed by some of the
    qr code scanners and introduced an app to handle scanning within django.

--- ticket_generator/urls.py
+++ ticket_generator/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('', include('tickets.urls')),
+    path('scanner/', include('scanner.urls')),
     path('theme/', include('theme.urls')),
 ]
 
