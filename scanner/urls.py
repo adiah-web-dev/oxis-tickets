46c5948fb4681a0cb2d762dee7218c9ab6724dcb
Nathaniel Adiah <nathanieladiah@gmail.com>
2025-06-04 08:17:17 -0400

    Start working on checking button

--- scanner/urls.py
+++ scanner/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('', views.scanner, name='scanner'),
 	# path('tickets/<uuid:pk>/', views.scanned_ticket, name='scanned-ticket'),
 	path('tickets/<uuid:pk>/', views.TicketDetailView.as_view(), name='scanned-ticket'),
+	path('tickets/<uuid:ticket_id>/check-in/', views.check_in, name='check-in'),
 ]
--- scanner/views.py
+++ scanner/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import generic
 
 from tickets.models import Ticket
@@ -11,6 +11,14 @@ def scanner(request):
 # def scanned_ticket(request, pk):
 # 	print(pk)
 # 	return render(request, 'scanner/ticket.html')
+
+def check_in(request, ticket_id):
+	# if request.method == 'POST':
+	# 	# print the status
+	# 	print(ticket_id)
+	# 	return redirect('scanned_ticket', pk=ticket_id)
+	print(ticket_id)
+	return redirect('scanned-ticket', pk=ticket_id)
 
 class TicketDetailView(generic.DetailView):
 	model = Ticket
